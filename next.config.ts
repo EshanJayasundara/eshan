ba867e09c235f66be6d93b7da2c85580c4687448
EshanJayasundara <e19163@eng.pdn.ac.lk>
2026-02-15 02:03:08 +0530

    Configure deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/eshan",
   images: {
+    unoptimized: true,
     remotePatterns: [],
   },
 };
